76c7fee9aaf27f26c79a4db000488513f49d2741
zmxlhj <2464844458@qq.com>
2020-05-07 17:05:38 +0800

    ww

--- js/lib/src/index.js
+++ js/lib/src/index.js
@@ -5,3 +5,25 @@
  * @FilePath     : \07wx_zhaopin\js\lib\src\index.js
  * @Description  : 
  */
+window.onload=function(){
+    //轮播图代码
+   
+   var swiperr = new Swiper('.swiper-container', {
+     spaceBetween: 9,
+     centeredSlides: true,
+     autoplay: {
+       delay: 4000,
+       disableOnInteraction: false,
+       color:"red",
+     },
+     pagination: {
+       el: '.swiper-pagination',
+       clickable: true,
+     },
+     navigation: {
+       nextEl: '.swiper-button-next',
+       prevEl: '.swiper-button-prev',
+     },
+     
+   });
+  }
